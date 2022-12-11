ec5d391799279cc171ce567b6e35e466ca52d3f3
w.cahyadi <w.cahyadi@verihubs.com>
2022-12-11 21:14:51 +0700

    [Quiz] finish quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,13 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n: i32) -> i32 {
+    if n > 40 {return n;}
+    n*2
+}
 
 // Don't modify this function!
 #[test]
